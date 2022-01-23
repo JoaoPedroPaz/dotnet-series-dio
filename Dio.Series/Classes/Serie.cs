7b91c52b1b4288e19e81497b428f7dd08a6c06fc
JoaoPedroPaz <jp.pfc@hotmail.com>
2022-01-22 23:23:47 -0300

    Projeto Parte 4

--- Dio.Series/Classes/Serie.cs
+++ Dio.Series/Classes/Serie.cs
@@ -12,6 +12,7 @@ namespace Dio.Series
 		private string Titulo { get; set; }
 		private string Descricao { get; set; }
 		private int Ano { get; set; }
+		private bool Excluido { get; set; }
 
 		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
 		{
@@ -20,6 +21,7 @@ namespace Dio.Series
 			this.Titulo = titulo;
 			this.Descricao = descricao;
 			this.Ano = ano;
+			this.Excluido = false;
 		}
 
 		public override string ToString()
@@ -40,6 +42,10 @@ namespace Dio.Series
 		public int retornaId()
 		{
 			return this.Id;
+		}
+		public void Excluir()
+		{
+			this.Excluido = true;
 		}
 
 	}
